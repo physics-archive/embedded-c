8d0bf572b41ab3cc67c4744f27c585c4f49e5a72
physics-archive <dirac>
2024-06-23 00:19:18 -0400

    vscode - MacOS/Windows

---
+++ .vscode/windows-template/.vs/firmware.c
@@ -0,0 +1,13 @@
+#include <include/libopencm3/stm32/f4/rcc.h>
+#include <libopencm3/include/stm32/f4/gpio.h>
+
+//Firmware 2024.06.18
+
+int main(void) {
+
+    while (1) {
+
+    }
+
+    return 0;
+}
