5d88f29dd93dbaf7cd334af7a999439bd9517ae8
physics-archive <dirac>
2024-06-23 20:55:47 -0400

    firmware work

--- firmware/src/firmware.c
+++ firmware/src/firmware.c
@@ -1,36 +1,60 @@
 //Firmware 2024.06.18
 #include <libopencm3/stm32/f4/rcc.h>
 #include <libopencm3/stm32/f4/gpio.h>
+#include <libopencm3/cm3/systick.h>
+#include <libopencm3/cm3/vector.h>
 
-#define LED_PORT (GPIOA)
-#define LED_PIN (GPIO5)
+
+#define LED_PORT      (GPIOA)
+#define LED_PIN       (GPIO5)
+
+#define CPU_FREQ      (84000000)
+#define systick_freq  (1000)
+
+////////////////////////////
+///   CONFIGURE PROGRAM   //
+////////////////////////////
+
+volatile uint64_t ticks = 0;
+void sys_tick_handler(void) {
+    ticks++;
+}
+
+static uint64_t get_ticks(void) {
+    return ticks;
+} 
+////////////////////////////
+///    SETUP FEATURES     //
+////////////////////////////
 
 static void rcc_setup(void) {
     rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_3V3_84MHZ]);
 }
 
 static void gpio_setup(void) {
-    // Turn on the clock for a given periphereal
     rcc_periph_clock_enable(RCC_GPIOA);
-
-    // Configure the LED pin. Send output to Port A
     gpio_mode_setup(LED_PORT,GPIO_MODE_OUTPUT,GPIO_PUPD_NONE,LED_PIN);
 }
 
-static void maintain_frequency_standard(uint32_t cycles) {
-    // 84MHZ cycles requires blank clock cycles
-    //At least 3-4 cpu cycles per loop. 
-    for (uint32_t i = 0; i < cycles; i++) {
-        __asm__("nop");
-    }
+static void systick_setup(void) {
+    systick_set_frequency(systick_freq,CPU_FREQ);
+    systick_counter_enable();
+    systick_interrupt_enable();
 }
 
+
+
+////////////////////////////
+///      APPLICATION      //
+////////////////////////////
+
+// Configure Microcontroller
 int main(void) {
     rcc_setup();
     gpio_setup();
 
+    //Execute Application
     while (1) {
-        // Turn the LED on and then off at freq (f')
         gpio_toggle(LED_PORT,LED_PIN);
         maintain_frequency_standard(84000000/4);
     }
