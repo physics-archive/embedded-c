9fbd38b413de34dcff928aeeccb7a3dac04842bf
physics-archive <dirac>
2024-06-26 15:37:36 -0400

    asm

--- asm/loop/whileloop.asm
+++ asm/loop/whileloop.asm
@@ -1,10 +1,10 @@
 Dump of assembler code for function main:
-   0x00000000 <+0>:     ldr     r0, [pc, #8]    @ (0xc <main+12>)
+   0x00000000 <+0>:     ldr     r3, [pc, #8]    @ (0xc <main+12>)
    0x00000002 <+2>:     nop
-   0x00000004 <+4>:     subs    r0, #1
+   0x00000004 <+4>:     subs    r3, #1
    0x00000006 <+6>:     bne.n   0x2 <main+2>
-   0x00000008 <+8>:     bx      lr
+   0x00000008 <+8>:     b.n     0x0 <main>
    0x0000000a <+10>:    nop
    0x0000000c <+12>:    ldr     r0, [r0, #116]  @ 0x74
    0x0000000e <+14>:    lsls    r0, r0, #5
-   End of assembler dump.
+End of assembler dump.
