b66a853ff31b3ba2f9c51ac3c071f3fec187447d
physics-archive <dirac>
2024-06-26 15:27:24 -0400

    forloop demo

--- asm/loop/forloop.c
+++ asm/loop/forloop.c
@@ -2,9 +2,11 @@
 
 int main(void) {
     uint32_t Nloop = 84000000 / 4;
-    for (uint32_t i = 0; i < Nloop; i++) {
-        __asm__("nop");
+    while (1) {
+        for (uint32_t i = 0; i < Nloop; i++) {
+            __asm__("nop");
+        }
+        uint32_t b = 40;
     }
-    
     return 0;
 }
